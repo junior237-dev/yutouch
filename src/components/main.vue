da6acd05795e6c2b8a1de6b4bae4fd013dc13222
juniordev237 <juniorndono21@gmail.com>
2021-10-05 08:45:17 +0100

    bannière secondaire réglée

--- src/components/main.vue
+++ src/components/main.vue
@@ -22,13 +22,15 @@
             <category-element v-for="i in 9" :key="i"/>
         </div>
     </div>
+
     <container-category-second v-for="i in 3" :key="i"/>
+
     <div class="banner-footer relative w-full mt-10">
-        <div class="banner2 w-full h-80 bg-cover bg-opacity-25 bg-center bg-no-repeat"></div>
-        <div class="description absolute h-80 top-0 left-0 right-0 bg-black bg-opacity-40">
-            <div class="propostions w-2/3 text-left mx-auto text-white text-base my-4">
+        <div class="banner2 w-full h-96 bg-cover bg-opacity-25 bg-center bg-no-repeat"></div>
+        <div class="description absolute h-96 top-0 left-0 right-0 bg-black bg-opacity-40">
+            <div class="propostions w-2/3 text-left mx-auto text-white my-8 sm:mt-10">
                 <h2 class="text-xl w-full mb-5">Proposez vos propres cours</h2>
-                <p>Lorem ipsum quia minima tenetur alias, accusamus commodi sequi odit vitae? Minima pariatur voluptatum tenetur minus porro voluptatibus at culpa.</p>
+                <p class="text-base">Lorem ipsum quia minima tenetur alias,quia minima tenetur alias,quia minima tenetur alias, accusamus commodi sequi odit vitae? Minima pariatur voluptatum tenetur minus porro voluptatibus at culpa.</p>
                 <button class="h-12 px-2 w-5/5 mx-auto mt-5 leaning-4 text-lg rounded-md bg-pink-700">Devenez Formateur</button>
             </div>
         </div>
@@ -59,6 +61,28 @@ export default {
     .banner2 {
         background-image: url(../assets/banner2.jpg);
         background-color: #232428;
+    }
+
+    @media screen and (min-width: 370px) {
+        .banner2 {
+            height: 300px
+        }
+        .description {
+            height: 300px
+        }
+    }
+
+    @media screen and (min-width: 600px) {
+        .banner2 {
+            height: 280px;
+        }
+        .description {
+            height: 280px
+        }
+
+        p.text-base {
+            max-width: 494px
+        }
     }
 
     @media screen and (max-width: 400px) {
