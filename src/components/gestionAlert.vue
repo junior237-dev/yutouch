1550efbe6f3c1c3e2e8dcc4c265e59d7b92e9409
juniordev237 <juniorndono21@gmail.com>
2021-10-19 11:45:18 +0100

    début du développement de gestionAlerte.vue

--- src/components/gestionAlert.vue
+++ src/components/gestionAlert.vue
@@ -1,5 +1,12 @@
 <template>
     <div class="gestionAlerte">
-        
+        <div class="location small text-gray-400 text-xs mt-3 mb-6 mx-0 pl-4">
+            <span>Vous êtes ici : </span>
+            <span class="link">Accueil Apprenant</span>
+            <span> > </span>
+            <span class="link">Profil</span>
+            <span> > </span>
+            <span class="link">Gestion Alerte</span>
+        </div>
     </div>
 </template>
